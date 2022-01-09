17de1e203ae9837c3f4bdd1ef3d8fae84346ef1b
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-09 14:21:00 +0300

    three more

--- problems_1.sql
+++ problems_1.sql
@@ -11,3 +11,16 @@ select distinct city from station where (city not like "a%" and city not like "e
 select distinct city from station where (city not like "a%" and city not like "e%" and city not like "i%" and
  city not like "o%" and city not like "u%") and (city not like "%a" and city not like "%e" and city not like "%i" 
  and city not like "%o" and city not like "%u")
+
+
+-- https://www.hackerrank.com/challenges/more-than-75-marks
+
+ SELECT NAME FROM STUDENTS WHERE MARKS>75 ORDER BY RIGHT(NAME,3),ID
+
+-- https://www.hackerrank.com/challenges/name-of-employees
+
+ SELECT NAME FROM EMPLOYEE ORDER BY NAME ASC
+
+-- https://www.hackerrank.com/challenges/salary-of-employees/
+
+ SELECT NAME FROM EMPLOYEE WHERE SALARY>2000 AND MONTHS<10
