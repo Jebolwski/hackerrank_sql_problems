c79737850db6eefdc5881f5b9ee623c1a5e40bdf
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-16 12:10:53 +0300

    two more

--- problems_3.sql
+++ problems_3.sql
@@ -1,3 +1,11 @@
 -- https://www.hackerrank.com/challenges/weather-observation-station-2/
 
 SELECT ROUND(SUM(LAT_N),2),ROUND(SUM(LONG_W),2) FROM STATION
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-13
+
+SELECT ROUND(SUM(LAT_N),4) FROM STATION WHERE LAT_N>38.7880 AND 137.2345>LAT_N
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-14
+
+SELECT ROUND(MAX(LAT_N),4) FROM STATION WHERE 137.2345>LAT_N
