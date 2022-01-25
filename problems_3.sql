67e8da57a05031f399f5273ada1427315b3e54c1
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-25 11:48:36 +0300

    one more

--- problems_3.sql
+++ problems_3.sql
@@ -70,3 +70,14 @@ select repeat('* ', @number := @number - 1) from information_schema.tables;
 
 set @number=0;
 select repeat('* ',@number:=@number+1) from information_schema.tables where @number<20;
+
+https://www.hackerrank.com/challenges/harry-potter-and-wands
+
+SELECT W.ID, WP.AGE, W.COINS_NEEDED, W.POWER 
+FROM WANDS AS W
+JOIN WANDS_PROPERTY AS WP
+ON (W.CODE = WP.CODE) 
+WHERE WP.IS_EVIL = 0 AND W.COINS_NEEDED = 
+(SELECT MIN(COINS_NEEDED) FROM WANDS AS X JOIN WANDS_PROPERTY AS Y ON (X.CODE = Y.CODE) 
+WHERE X.POWER = W.POWER AND Y.AGE = WP.AGE) 
+ORDER BY W.POWER DESC, WP.AGE DESC;
