0a4bfcff7984e1c4c7d4acf61517c40e6426d02d
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-28 14:04:48 +0300

    one more

--- problems_3.sql
+++ problems_3.sql
@@ -104,3 +104,9 @@ from Hackers h inner join (select s.hacker_id,max(score) as sscore from Submissi
 group by h.hacker_id,h.name
 having sum(sscore)>0
 order by sum(sscore) desc,h.hacker_id asc;
+
+-- https://www.hackerrank.com/challenges/the-company
+
+select c.company_code, c.founder, count(distinct lm.lead_manager_code), count(distinct sm.senior_manager_code), count(distinct m.manager_code), count(distinct e.employee_code) from Company c, Lead_Manager lm, Senior_Manager sm, Manager m, Employee e
+where c.company_code = lm.company_code and lm.lead_manager_code = sm.lead_manager_code and sm.senior_manager_code = m.senior_manager_code and m.manager_code = e.manager_code group by c.company_code, c.founder
+order by c.company_code
